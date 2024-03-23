33c4e5e25de75681d20b516c7098c149d7a8efa6
mozidan27 <153581941+mozidan27@users.noreply.github.com>
2024-03-23 17:53:12 +0200

    small update on full screen

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:myapp/widgets/full_screen_image.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key, Key});
@@ -39,38 +40,6 @@ class HomePage extends StatelessWidget {
               borderRadius: BorderRadius.circular(16),
               child: Image.asset('asset/Image/image${index + 1}.JPG'),
             ),
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class FullScreenImage extends StatelessWidget {
-  final String imagePath;
-
-  const FullScreenImage({required this.imagePath, super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'shayf el3zmaa',
-          style: TextStyle(
-            color: Colors.black,
-            fontFamily: 'ADAM',
-          ),
-        ),
-      ),
-      body: Center(
-        child: GestureDetector(
-          onTap: () => Navigator.pop(context),
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: ClipRRect(
-                borderRadius: BorderRadius.circular(32),
-                child: Image.asset(imagePath)),
           ),
         ),
       ),
---
+++ lib/widgets/full_screen_image.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class FullScreenImage extends StatelessWidget {
+  final String imagePath;
+
+  const FullScreenImage({required this.imagePath, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'shayf el3zmaa',
+          style: TextStyle(
+            color: Colors.black,
+            fontFamily: 'ADAM',
+          ),
+        ),
+      ),
+      body: Center(
+        child: GestureDetector(
+          onTap: () => Navigator.pop(context),
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ClipRRect(
+                borderRadius: BorderRadius.circular(32),
+                child: Image.asset(imagePath)),
+          ),
+        ),
+      ),
+    );
+  }
+}
